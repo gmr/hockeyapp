131cba9d807e6b856e2b4426a37bb055a42558de
Max Thoursie <max@spotify.com>
2013-06-17 16:50:43 +0200

    Replace optparse with argparse, use subcommands
    Argparse supports subcommands, which makes it easier to group options
    with their related action.
    
    The actions list-applications, list-users, list-craches, add-app-user
    and detail are now subcommands.
    
    One downside is that python 2.7 argparse does not support aliases, so no
    short versions of the above list are implemented. (previous short options -a,
    -u, -c and -d does not have any new equivialent)

--- hockeyapp/cli.py
+++ hockeyapp/cli.py
@@ -7,7 +7,7 @@ __email__ = 'gmr@myyearbook.com'
 __since__ = '2011-09-12'
 
 import logging
-import optparse
+import argparse
 import sys
 
 from . import __version__
@@ -16,6 +16,7 @@ from . import apps
 from . import crashes
 from . import crashlog
 from . import team
+
 
 def print_api_error(e):
     """ Print an APIError exception to stderr
@@ -26,150 +27,102 @@ def print_api_error(e):
     """
     sys.stderr.write("\nERROR: %s\n" % str(e))
 
-def parse_options():
-    """Parse commandline options.
 
-    :returns: Tuple of OptionParser object, options and arguments
+def parse_args():
+    """Parse commandline arguments.
+
+    :returns: Tuple of OptionParser object and arguments
     """
-    usage = "usage: %prog -k <api_key> [options [-i app_id] [-d crash_id]]"
-    version_string = "%%prog %s" % __version__
+    version_string = "%%(prog)s %s" % __version__
     description = "Hockeyapp API Client"
 
-    # Create our parser and setup our command line options
-    parser = optparse.OptionParser(usage=usage,
-                                   version=version_string,
-                                   description=description)
+    # Create our parser and setup our command line args
+    parser = argparse.ArgumentParser(description=description)
 
-    parser.add_option("-k", "--api-key",
-                      action="store", dest="api_key",
-                      help="Supply the API Token from hockeyapp.net")
+    parser.add_argument("-V", "--version", action='version',
+                        version=version_string)
+    parser.add_argument("-v", "--verbose",
+            action="store_true", dest="verbose",
+            help="turn on debug mode")
 
-    parser.add_option("-a", "--list-applications",
-                      action="store_true", dest="list_applications",
-                      help="List the applications available at HockeyApp")
+    parser.add_argument("-k", "--api-key",
+            dest="api_key", required=True,
+            help="Supply the API Token from hockeyapp.net")
 
-    parser.add_option("-u", "--list-users",
-                      action="store_true", dest="list_users",
-                      help="List users associated with the specified HockeyApp")
+    # Define subcommands
+    subparsers = parser.add_subparsers(title="commands", metavar="COMMAND",
+            description="See '%(prog)s COMMAND -h' for more information on a specific command.")
 
-    parser.add_option("-c", "--list-crashes",
-                      action="store_true", dest="list_crashes",
-                      help="List crashes associated with the specified HockeyApp")
+    def show(request):
+        print request.execute()
 
-    parser.add_option("--add-app-user",
-                      action="store_true", dest="add_app_user",
-                      help="Add a user to a HockeyApp")
+    la = subparsers.add_parser('list-applications',
+            help="List the applications available at HockeyApp")
+    la.set_defaults(func=lambda a:
+            show(apps.AppList(a.api_key)))
 
-    parser.add_option("-o", "--offset",
-                      action="store", dest="offset", default=1,
-                      help="Use an offset for the crash list")
+    lu = subparsers.add_parser('list-users',
+            help="List users associated with the specified HockeyApp")
+    lu.set_defaults(func=lambda a:
+            show(team.AppUsers(a.api_key, a.app_id)))
 
-    parser.add_option("-i", "--app-id",
-                      action="store", dest="app_id",
-                      help="The application identifier at HockeyApp")
+    lc = subparsers.add_parser('list-crashes',
+            help="List crashes associated with the specified HockeyApp")
+    lc.set_defaults(func=lambda a:
+            show(crashes.CrashList(a.api_key, a.app_id, a.offset)))
 
-    parser.add_option("-d", "--detail",
-                      action="store", dest="detail",
-                      help="Get the detail for a crash ID at HockeyApp")
+    aau = subparsers.add_parser("add-app-user",
+            help="Add a user to a HockeyApp")
+    aau.set_defaults(func=lambda a:
+            show(team.AppAddUser(a.api_key, a.app_id, a.email)))
 
-    parser.add_option("-e", "--email",
-                      action="store", dest="email",
-                      help="User email address")
+    d = subparsers.add_parser("detail",
+            help="Get the detail for a crash ID at HockeyApp")
+    d.set_defaults(func=lambda a:
+            show(crashlog.CrashLog(a.api_key, a.app_id, a.crash_id, a.mode)))
 
-    parser.add_option("-m", "--mode",
-                      action="store", dest="mode", default="text",
-                      help="Set the mode for retreiving the detail " + 
-                           "for a crash [log, text]")
+    # Arguments common for many actions
+    for p in (lu, lc, aau, d):
+        p.add_argument("app_id",
+                       help="The application identifier at HockeyApp")
 
-    parser.add_option("-v", "--verbose",
-                      action="store_true", dest="verbose", default=False,
-                      help="Turn on debug mode")
+    # Command specific arguments
+    lc.add_argument("-o", "--offset",
+            default=1,
+            help="Use an offset for the crash list")
 
-    options, arguments = parser.parse_args()
-    return parser, options, arguments
+    aau.add_argument("email",
+            help="User email address")
+
+    d.add_argument("crash_id",
+            help="The crash ID")
+    d.add_argument("-m", "--mode",
+            default="text", choices=['log', 'text'],
+            help="Set the mode for retreiving the detail for a crash")
+
+    # Print help message when there's no subcommand
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
+
+    arguments = parser.parse_args()
+    return parser, arguments
 
 
 def main():
     """Main commandline invocation function."""
-    # Parse our options and arguments
-    parser, options, args = parse_options()
+    # Parse our and arguments
+    parser, args = parse_args()
 
     # If debugging is turned on, turn on logging and set the level
-    if options.verbose:
+    if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
 
-    # Make sure we have an api key
-    if not options.api_key:
-        sys.stderr.write('\nERROR: Missing API Key\n\n')
-        parser.print_help()
-        sys.exit(1)
+    try:
+        args.func(args)
+    except APIError as e:
+        print_api_error(e)
 
-    if options.list_applications:
-        app_list = apps.AppList(options.api_key)
-        try:
-            print app_list.execute()
-        except APIError as e:
-            print_api_error(e)
-
-        return
-
-    if options.list_users:
-        if not options.app_id:
-            sys.stderr.write('\nERROR: Missing App ID\n\n')
-            parser.print_help()
-            sys.exit(1)
-
-        user_list = team.AppUsers(options.api_key, options.app_id)
-        try:
-            print user_list.execute()
-        except APIError as e:
-            print_api_error(e)
-
-        return
-
-    if options.add_app_user:
-        if not options.app_id:
-            sys.stderr.write('\nERROR: Missing App ID\n\n')
-            parser.print_help()
-            sys.exit(1)
-
-        if not options.email:
-            sys.stderr.write('\nERROR: Missing Email Address\n\n')
-            parser.print_help()
-            sys.exit(1)
-
-        add_user = team.AppAddUser(options.api_key, options.app_id, options.email)
-        try:
-            print add_user.execute()
-        except APIError as e:
-            print_api_error(e)
-
-        return
-
-    if options.list_crashes:
-        crash_list = crashes.CrashList(options.api_key, options.app_id, options.offset)
-        try:
-            print crash_list.execute()
-        except APIError as e:
-           print_api_error(e)
-
-        return
-
-    if options.detail:
-        crash_detail = crashlog.CrashLog(options.api_key,
-                                         options.app_id,
-                                         options.detail,
-                                         options.mode)
-        try:
-            print crash_detail.execute()
-        except APIError as e:
-            sys.stderr.write("\nERROR: %s\n\n" % str(e))
-
-        return
-
-    sys.stderr.write('\nERROR: You must select an action to take\n\n')
-    parser.print_help()
-    sys.exit(1)
 
 if __name__ == '__main__':
     main()
