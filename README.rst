be917cd54620bfedbc6ed6f69740af04c633b295
Gavin M. Roy <gmr@meetme.com>
2013-10-16 10:55:41 -0400

    Package metadata updates
    - Add LICENSE file
    - Add MANIFEST.in
    - Update setup.py to use README.rst for long description
    - Update setup.py classifiers
    - Update setup.py to include LICENSE content
    - Update description and URL in setup.py
    - Update testing information in setup.py

---
+++ README.rst
@@ -0,0 +1,56 @@
+HockeyApp.net Python Client
+===========================
+Access your crash log and crash detail data for iOS applications tracked at
+hockeyapp.net.
+
+|PyPI version| |Downloads| |Build Status|
+
+CLI Usage
+---------
+
+        ./hockeyapp-cli
+
+        usage: hockeyapp-cli [-h] [-V] [-v] -k API_KEY COMMAND ...
+
+        Hockeyapp API Client
+
+        optional arguments:
+          -h, --help            show this help message and exit
+          -V, --version         show program's version number and exit
+          -v, --verbose         turn on debug mode
+          -k API_KEY, --api-key API_KEY
+                                Supply the API Token from hockeyapp.net
+
+        commands:
+          See 'hockeyapp-cli COMMAND -h' for more information on a specific command.
+
+          COMMAND
+            list-applications   List the applications available at HockeyApp
+            list-users          List users associated with the specified HockeyApp
+            list-crashes        List crashes associated with the specified HockeyApp
+            add-app-user        Add a user to a HockeyApp
+            detail              Get the detail for a crash ID at HockeyApp
+            list-versions       List the versions of an app
+            version-delete      Delete a specified version
+            version-add         Add a new version of an app
+
+Example usage
+-------------
+
+        import hockeyapp
+
+        token = 'Your API Key'
+        public_identifier = 'Public identifier value for the app'
+        crash_id = 1234
+
+        app = hockeyapp.Application(token)
+        print app.list()
+        print app.statistics(public_identifier)
+
+
+.. |PyPI version| image:: https://badge.fury.io/py/hockeyapp.png
+   :target: http://badge.fury.io/py/hockeyapp
+.. |Downloads| image:: https://pypip.in/d/hockeyapp/badge.png
+   :target: https://crate.io/packages/hockeyapp
+.. |Build Status| image:: https://travis-ci.org/gmr/hockeyapp.png?branch=master
+   :target: https://travis-ci.org/gmr/hockeyapp
--- setup.py
+++ setup.py
@@ -11,19 +11,34 @@ if sys.version_info < (2, 7, 0):
 
 console_scripts = ['hockeyapp-cli=hockeyapp.cli:main']
 
-long_description = """Python client for the HockeyApp.net API"""
 core.setup(name='hockeyapp',
            version=__version__,
-           description="HockeyApp.net API",
-           long_description=long_description,
-           classifiers=[
-            'Development Status :: 4 - Beta',
-           ],
+           description='Python client for the HockeyApp.net API',
+           long_description=open('README.rst').read(),
            author='Gavin M. Roy',
            author_email='gavinmroy@gmail.com',
-           url='https://github.com/gmr/hockeyapp',
+           url='http://hockeyapp.readthedocs.org',
            packages=['hockeyapp'],
            entry_points=dict(console_scripts=console_scripts),
            zip_safe=True,
+           package_data={'': ['LICENSE', 'README.md']},
+           include_package_data=True,
            install_requires=requirements,
-           tests_require=tests_require)
+           tests_require=tests_require,
+           test_suite='nose.collector',
+           license=open('LICENSE').read(),
+           classifiers=['Development Status :: 4 - Beta',
+                        'Intended Audience :: Developers',
+                        'License :: OSI Approved :: BSD License',
+                        'Operating System :: OS Independent',
+                        'Programming Language :: Python :: 2',
+                        'Programming Language :: Python :: 2.6',
+                        'Programming Language :: Python :: 2.7',
+                        'Programming Language :: Python :: 3',
+                        'Programming Language :: Python :: 3.2',
+                        'Programming Language :: Python :: 3.3',
+                        'Programming Language :: Python :: Implementation :: CPython',
+                        'Programming Language :: Python :: Implementation :: PyPy',
+                        'Topic :: Communications',
+                        'Topic :: Internet',
+                        'Topic :: Software Development :: Libraries'])
